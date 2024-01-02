fb8495ef315ab9ff07f4b32e298a3ea5ddf63970
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-02 19:24:39 +0100

    ah5

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
+from os import getenv
 from models.base_model import BaseModel, Base
 from models import hbnb_type_storage
 from sqlalchemy import Column, String
@@ -12,7 +13,7 @@ class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
 
-    if hbnb_type_storage == 'db':
+    if getenv("HBNB_TYPE_STORAGE") == 'db':
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
