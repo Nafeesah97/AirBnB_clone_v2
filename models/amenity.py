463043e83f6c2f98ecfc3de47ee3f2396520bfae
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-02 15:41:49 +0100

    ah3

--- models/amenity.py
+++ models/amenity.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-"""This is the amenity class"""
+""" State Module for HBNB project """
+from os import getenv
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 class Amenity(BaseModel, Base):
-    """This is the class for Amenity
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "amenities"
-    name = Column(String(128), nullable=False
-    place_amenities = None  # Placeholder for circular import resolution
-
+    """ Representation of amenities """
+    __tablename__ = 'amenities'
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        @property
-        def place_amenities(self):
-            '''
-            Returns the relationship with Place and
-            resolves circular import issue
-            '''
-            from models.place import place_amenity
-            return relationship("Place", secondary=place_amenity)
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                        viewonly=False)
+    else:
+        name = ""
