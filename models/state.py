f984cdce54c67f0739528af236dcbcaa53673a81
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-03 19:30:24 +0100

    cr8

--- models/state.py
+++ models/state.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from models import storage
 from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
@@ -19,6 +18,7 @@ class State(BaseModel, Base):
 
         @property
         def cities(self):
+            from models import storage
             '''returns the list of City instances with state_id
                 equals the current State.id
                 FileStorage relationship between State and City
