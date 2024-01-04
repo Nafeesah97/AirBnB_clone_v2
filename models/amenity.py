1a8540387b24fbf19be09c0964bca28223fd8621
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-04 02:24:19 +0100

    cr17

--- models/amenity.py
+++ models/amenity.py
@@ -1,28 +1,17 @@
 #!/usr/bin/python3
-""" Amenity Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Table
-from sqlalchemy.orm import relationship
+""" State Module for HBNB project """
 from os import getenv
-
-if getenv("HBNB_TYPE_STORAGE") == 'db':
-    place_amenity = Table('place_amenity', Base.metadata,
-                          Column('place_id', String(60),
-                                 ForeignKey('places.id'),
-                                 primary_key=True,
-                                 nullable=False),
-                          Column('amenity_id', String(60),
-                                 ForeignKey('amenities.id'),
-                                 primary_key=True,
-                                 nullable=False)
-                          )
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel, Base):
-    """ Amenity class to store Amenity information """
+    """ Representation of amenities """
     __tablename__ = 'amenities'
-
-    name = Column(String(128), nullable=False)
-
     if getenv("HBNB_TYPE_STORAGE") == 'db':
-        place_amenities = relationship('Place', secondary="place_amenity")
+        name = Column(String(128), nullable=False)
+        place_amenities = relationship("Place", secondary="place_amenity",
+                                        viewonly=False)
+    else:
+        name = ""
