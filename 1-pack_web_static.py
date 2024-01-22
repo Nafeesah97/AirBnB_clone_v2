2336c219b061b70ce204aefc7b5660ffe6312ceb
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-22 15:29:21 +0100

    1cr1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""import necessary libraries"""
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    
+    local("mkdir -p versions")
+
+    now = datetime.utcnow()
+    date_str = now.strftime("%Y%m%d%H%M%S")
+
+    archive_name = "web_static_{}.tgz".format(date_str)
+
+    result = local("tar -czvf versions/{} web_static".format(archive_name), capture=True)
+
+    if result.failed:
+        return None
+    else:
+        archive_path = os.path.join("versions", archive_name)
+        print("web_static packed: {} -> {}Bytes".format(archive_path, os.path.getsize(archive_path)))
+        return archive_path
