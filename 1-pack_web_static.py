e1b6e91dc48bdd298a55f5aa1c019d655a014e18
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-22 06:54:28 -0800

    1cr2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,9 +4,10 @@ from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
     """Generates a .tgz archive from the contents of the web_static folder."""
-    
+
     local("mkdir -p versions")
 
     now = datetime.utcnow()
@@ -14,11 +15,14 @@ def do_pack():
 
     archive_name = "web_static_{}.tgz".format(date_str)
 
-    result = local("tar -czvf versions/{} web_static".format(archive_name), capture=True)
+    result = local(
+            "tar -czvf versions/{} web_static".format(archive_name),
+            capture=True)
 
     if result.failed:
         return None
     else:
         archive_path = os.path.join("versions", archive_name)
-        print("web_static packed: {} -> {}Bytes".format(archive_path, os.path.getsize(archive_path)))
+        print("web_static packed: {} -> {}Bytes".format(
+            archive_path, os.path.getsize(archive_path)))
         return archive_path
