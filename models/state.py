82b9e29ce18cd3a89826ce28e954781780ed82ad
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-24 00:56:40 +0100

    updating modelgt

--- models/state.py
+++ models/state.py
@@ -12,10 +12,10 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='delete')
+        _cities_relationship = relationship('City', backref='state', cascade='delete')
 
         @property
-        def cities(self):
+        def _get_cities(self):
             from models import storage
             '''returns the list of City instances with state_id
                 equals the current State.id
