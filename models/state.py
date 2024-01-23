0bb91222c5a01d77ae0e07b0c8414ad94a0fa179
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-24 00:52:14 +0100

    updating model3

--- models/state.py
+++ models/state.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
 from models.city import City
-import shlex
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    _cities_relationship = relationship("City", cascade='all, delete, delete-orphan', backref="state")
+    """ State class """
+    __tablename__ = 'states'
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='delete')
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if city[0] == 'City':
-                lista.append(var[key])
-        for elem in lista:
-            if elem.state_id == self.id:
-                result.append(elem)
-        return result
+        @property
+        def cities(self):
+            from models import storage
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            _cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    _cities.append(city)
+            return _cities
