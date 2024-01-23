866c0949db73fc77899a8e7eb3c50d460241182e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-24 00:42:35 +0100

    updating model

--- models/state.py
+++ models/state.py
@@ -13,19 +13,24 @@ class State(BaseModel, Base):
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state', cascade='delete')
-    else:
-        name = ''
 
         @property
         def cities(self):
             from models import storage
+            import shlex
             '''returns the list of City instances with state_id
                 equals the current State.id
                 FileStorage relationship between State and City
             '''
-            _cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    _cities.append(city)
-            return _cities
+            var = storage.all()
+            lista = []
+            result = []
+            for key in var:
+                city = key.replace('.', ' ')
+                city = shlex.split(city)
+                if (city[0] == 'City'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.state_id == self.id):
+                    result.append(elem)
+            return (result)
